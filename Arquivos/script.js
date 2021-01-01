b256abccaf60d0b98a409ecfcb8fe0df7389663f
diou <74876641+Diounata@users.noreply.github.com>
2021-01-01 17:09:38 -0300

    Atualização: Style do site
    Substituição do CSS puro pelas classes Bootstrap.

--- Arquivos/script.js
+++ Arquivos/script.js
@@ -1,34 +1,34 @@
-var button_verificar = document.getElementById('button-verificar')
-var button_reset = document.getElementById('button-reset')
-button_verificar.addEventListener('click', verificar)
-button_reset.addEventListener('click', resetar)
+var resultado = document.querySelector('div.resultado')
 
 function verificar() {
-    var resultado = document.getElementById('resultado')
-    var input_dinheiro = Number(document.getElementById('input-money').value)
+    var input_dinheiro = Number(document.querySelector('input.input-dinheiro').value)
     var dinheiro_amostra = [200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01]
     var dinheiro = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     var contagem = input_dinheiro
 
-    resultado.innerHTML = `<br/> <p> Valor inserido: R$${input_dinheiro.toFixed(2).replace('.', ',')} </p> <br/>`
+    resultado.innerHTML = `<p class="fs-5 mt-2"> • Valor inserido: R$${input_dinheiro.toFixed(2).replace('.', ',')} </p>`
 
-    // Adicionar quantidade de cédulas/moedas que foram definidas
-    for (var pos in dinheiro) {
-        while (contagem.toFixed(2) >= dinheiro_amostra[pos]) {
-            dinheiro[pos]++
-            contagem = contagem.toFixed(2) - dinheiro_amostra[pos]
-        }
+    // Resolver possíveis erros
+    if (input_dinheiro <= 0) {
+        resultado.innerHTML = `<p class="alert alert-danger my-1"> [ERRO] O valor inserido é inválido. Tente novamente </p>`
+    } else {
+        // Adicionar quantidade de cédulas/moedas que foram definidas
+        for (var pos in dinheiro) {
+            while (contagem.toFixed(2) >= dinheiro_amostra[pos]) {
+                dinheiro[pos]++
+                contagem = contagem.toFixed(2) - dinheiro_amostra[pos]
+            }
 
-
-        // Inserir as cédulas/moedas na página
-        for (var timer = dinheiro[pos]; timer > 0; timer = timer - 1) {
-            resultado.innerHTML += `<img src="imagens/dinheiro_valor_${dinheiro_amostra[pos]}.jpg"/> `
+            // Inserir as cédulas/moedas na página
+            for (var timer = dinheiro[pos]; timer > 0; timer = timer - 1) {
+                resultado.innerHTML += `<img src="imagens/dinheiro_valor_${dinheiro_amostra[pos]}.jpg" class="me-1 mb-1">`
+            }
         }
     }
 }
 
 function resetar() {
-    resultado.innerHTML = ``
+    resultado.innerHTML = ''
 }
 
 // Cédulas: 200[0], 100[1], 50[2], 20[3], 10[4], 5[5], 2[6]
